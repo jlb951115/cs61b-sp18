1b48ee2c6f3599be796264a5b70f73b3a7ddfa9f
jlb951115 <3065649450@qq.com>
2023-02-20 01:18:45 +0800

    finish the proj1a but waiting the test code

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -32,7 +32,7 @@ public class ArrayDeque<Item>
         if (size == 0)
             return null;
         else
-            return items[nextlast-1];
+            return items[minusone(nextlast,items.length)];
     }
 
     public Item getFirst()
@@ -40,16 +40,88 @@ public class ArrayDeque<Item>
         if (size == 0)
             return null;
         else
-            return items[nextfirst+1];
+            return items[plusone(nextfirst,items.length)];
     }
-    private void resize(int capticy)
+
+    private void resize(int cap)
     {
-        if (nextfirst+1 < nextlast-1)
+        Item[] a = (Item[]) new Object[cap];
+        int number = size;
+        int i = 0;
+        int index = nextfirst;
+        while (number > 0)
+        {
+            a[i] = items[plusone(index,items.length)];
+            i++;
+            number--;
+            index = plusone(index,items.length);
+        }
+        items = a;
+        nextfirst = minusone(0,items.length);
+        nextlast = size;
+    }
+    private int minusone(int index, int capt)
+    {
+        if (index - 1 < 0)
+            return capt - 1;
+        else
+            return index - 1;
+    }
+
+    private int plusone(int index, int capt)
+    {
+        if (index + 1 == capt)
+            return 0;
+        else
+            return index + 1;
     }
     public void addFirst(Item item)
     {
+        if (size == items.length)
+            resize(size * 2);
         items[nextfirst] = item;
+        nextfirst = minusone(nextfirst,items.length);
         size += 1;
-        nextfirst -= 1;
+    }
+
+    public void addLast(Item item)
+    {
+        if (size == items.length)
+            resize(size * 2);
+        items[nextlast] = item;
+        nextlast = plusone(nextlast, items.length);
+        size += 1;
+    }
+
+    public Item removeFirst()
+    {
+        if (size == 0)
+            return null;
+        size -= 1;
+        nextfirst = plusone(nextfirst, items.length);
+        return items[nextfirst];
+    }
+
+    public Item removeLast()
+    {
+        if (size == 0)
+            return null;
+        size -= 1;
+        nextlast = minusone(nextlast,items.length);
+        return items[nextlast];
+    }
+
+    public void printDeque()
+    {
+        int i = 0;
+        int index = nextfirst;
+        while(i < size)
+        {
+            System.out.print(items[plusone(index,items.length)]);
+            System.out.print(" ");
+            index = plusone(index,items.length);
+            i++;
+        }
+        System.out.print("\n");
     }
 }
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,15 @@
+public class ArrayDequeTest {
+    public static void main(String[] args)
+    {
+        ArrayDeque<String> lld = new ArrayDeque<String>();
+        lld.addLast("a");
+        lld.addLast("b");
+        lld.addFirst("c");
+        lld.addLast("d");
+        lld.addLast("e");
+        lld.addFirst("f");
+        lld.addLast("g");
+        lld.addLast("h");
+        lld.addLast("z");
+    }
+}
