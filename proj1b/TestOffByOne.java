affbd75fb444a934bc11ab2b5259bcad79fe7c0f
jlb951115 <3065649450@qq.com>
2023-02-25 02:31:01 +0800

    adding some test example

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -15,5 +15,8 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('A', 'a'));
+        assertFalse(offByOne.equalChars('A', 'a'));
+        assertTrue(offByOne.equalChars('&', '%'));
     }
 }
