aaf5ffe26a8049b82c245625e84677883f8bac0b
jlb951115 <3065649450@qq.com>
2023-02-27 17:13:06 +0800

    completely right 12 gold points

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,7 +7,7 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<Integer>();
         String s = "";
         int lowerbounbder = 5;
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++){
             double number = StdRandom.uniform();
             int x = StdRandom.uniform(lowerbounbder, lowerbounbder+5);
             if (number >= 0.5){
